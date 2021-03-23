f06d590424bd5cbcd6de395813e929cef139d563
Logan Lu <luchangda_njfu@163.com>
2021-03-23 22:55:25 +0800

    作业展示代码

--- iris.py
+++ iris.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import train_test_split;
 # 1数据库信息
 iris_dataset = load_iris();
 print(iris_dataset['DESCR'][:193] + "n\...");
-print("##############################################################################################################################")
+print("------------------------------------------------------------------------------------------------------------------------------")
 print("1.1数据组键名:{}".format(iris_dataset.keys()));
 print("1.2.预测花品种:{}".format(iris_dataset['target_names']));
 print("1.3特征名称:{}".format(iris_dataset['feature_names']));
@@ -23,7 +23,7 @@ print("1.6数据前5行:\n{}".format(iris_dataset['data'][:5]));
 print("1.7目标数据类型:{}".format(type(iris_dataset['target'])));
 print("1.8目标数据格式:{}".format(iris_dataset['target'].shape));
 print("1.9目标:\n{}".format(iris_dataset['target']));
-print("##############################################################################################################################")
+print("------------------------------------------------------------------------------------------------------------------------------")
 
 #2.训练集&测试集
 X_train, X_test, y_train, y_test = train_test_split(
@@ -32,38 +32,37 @@ print("2.1数据X训练集格式:{}".format(X_train.shape));
 print("2.2标签y训练集格式:{}".format(y_train.shape));
 print("2.3数据X测试集格式:{}".format(X_train.shape));
 print("2.4标签y测试集格式:{}".format(y_test.shape));
-print("##############################################################################################################################")
+print("------------------------------------------------------------------------------------------------------------------------------")
 
 # 数据可视化
 
 # 用X_train创建数据库，并对数据列进行标记
 iris_dataframe = pd.DataFrame(X_train,columns=iris_dataset.feature_names)
-
 grr = pd.plotting.scatter_matrix(
     iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
     hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3);
 plt.show()
 
+
 # k临近算法
 knn = KNeighborsClassifier(n_neighbors=1);
 knn.fit(X_train, y_train);
 
-#做出预测
+#3.做出预测
 X_new = np.array([[5, 2.9, 1, 0.2]]);
-print("X_new的格式:{}".format(X_new.shape));
-
+print("3.1X_new的格式:{}".format(X_new.shape));
 #调用knn对象的predict方法进行预测
 prediction = knn.predict(X_new);
-print("Prediction:{}".format(prediction));
-print("预测目标姓名:{}".format(
+print("3.2Prediction:{}".format(prediction));
+print("3.3预测目标姓名:{}".format(
     iris_dataset['target_names'][prediction]
 ));
-print("##############################################################################################################################")
+print("------------------------------------------------------------------------------------------------------------------------------")
 
 #评估模型
 y_pred = knn.predict(X_test);
-print("测试集预测结果:\n{}".format(y_pred));
-print("测试集得分:{:.2f}".format(np.mean(y_pred == y_test)));
-print("测试集得分:{:.2f}".format(knn.score(X_test,y_test)));
-print("##############################################################################################################################")
+print("4.1测试集预测结果:\n{}".format(y_pred));
+print("4.2测试集得分:{:.2f}".format(np.mean(y_pred == y_test)));
+print("4.3测试集得分:{:.2f}".format(knn.score(X_test,y_test)));
+print("------------------------------------------------------------------------------------------------------------------------------")
 
